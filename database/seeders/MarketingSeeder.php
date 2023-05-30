b18f0570e8cdcc5d598b1204af82023f279b81c4
mhambali920 <mhambali920@gmail.com>
2023-05-30 12:40:37 +0700

    add seeder for table marketings

---
+++ database/seeders/MarketingSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MarketingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('marketings')->insert([
+            ['name' => 'Alfandy'],
+            ['name' => 'Mery'],
+            ['name' => 'Danang'],
+        ]);
+    }
+}
