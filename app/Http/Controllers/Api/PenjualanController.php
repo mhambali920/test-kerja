53064351048879fa375e951426864f2a1dd68536
mhambali920 <mhambali920@gmail.com>
2023-05-30 14:33:19 +0700

    membuat controller penjualan dan route

--- app/Http/Controllers/Api/PenjualanController.php
+++ app/Http/Controllers/Api/PenjualanController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Helpers\ResponseFormatter;
 use App\Models\Penjualan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use App\Http\Controllers\Controller;
+use Exception;
 
 class PenjualanController extends Controller
 {
@@ -24,6 +27,30 @@ class PenjualanController extends Controller
 
     public function index()
     {
-        $penjualan =  Penjualan::with('marketing');
+
+        try {
+            $penjualan = Penjualan::with('marketing')
+                ->selectRaw('marketing_id, MONTH(date) as month, SUM(total_balance) as total')
+                ->groupBy('marketing_id', 'month')
+                ->get();
+
+            $result = $penjualan->map(function ($item) {
+                $omzet = $item->total;
+                $komisi = $this->hitungKomisi($omzet);
+                $komisiNominal = $omzet * $komisi;
+
+                return [
+                    'Marketing' => $item->marketing->name,
+                    'Bulan' => Carbon::createFromFormat('n', $item->month)->translatedFormat('F'),
+                    'Omzet' => $omzet,
+                    'Komisi %' => $komisi * 100 . '%',
+                    'Komisi Nominal' => $komisiNominal,
+                ];
+            });
+
+            return ResponseFormatter::success(data: $result, message: 'success');
+        } catch (Exception $e) {
+            return ResponseFormatter::error(message: $e->getMessage(), code: 500);
+        }
     }
 }
